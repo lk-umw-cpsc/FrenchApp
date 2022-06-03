ddd1f42f5de4bd6c027fc3b1384c7baac443ac40
Lauren Knight <lknight2@mail.umw.edu>
2022-06-03 18:04:38 -0400

    whitespace

--- ConjugationFrame.java
+++ ConjugationFrame.java
@@ -4,6 +4,7 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
