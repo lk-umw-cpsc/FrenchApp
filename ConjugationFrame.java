ab47c7d9145d0a02da4a26d59b8a725031652b98
Lauren Knight <lknight2@mail.umw.edu>
2022-06-03 16:22:02 -0400

    begin basic format of conjugation frame

--- ConjugationFrame.java
+++ ConjugationFrame.java
@@ -3,7 +3,10 @@ import java.awt.event.WindowListener;
 
 import javax.swing.Box;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+import javax.swing.border.EmptyBorder;
 
 public class ConjugationFrame extends JFrame implements WindowListener {
     
@@ -13,11 +16,53 @@ public class ConjugationFrame extends JFrame implements WindowListener {
         super("Pratiquer les conjugaisons");
         this.parent = parent;
 
-        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        if (parent != null) {
+            setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        } else {
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        }
 
         Box rowContainer = Box.createVerticalBox();
+        rowContainer.setBorder(new EmptyBorder(16, 16, 16, 16));
+        
+        Box row;
+        
+        row = Box.createHorizontalBox();
+            // row.add(Box.createHorizontalGlue());
+            row.add(new JLabel("<html><body style='text-align: center'>Conjugate <i>manger</i></body></html>"));
+            // row.add(Box.createHorizontalGlue());
+        rowContainer.add(row);
 
+        row = Box.createHorizontalBox();
+            row.add(new JLabel("je "));
+            row.add(new JTextField(8));
+            row.add(Box.createHorizontalGlue());
+            row.add(Box.createHorizontalStrut(32));
+            row.add(new JLabel("nous "));
+            row.add(new JTextField(8));
+        rowContainer.add(row);
 
+        row = Box.createHorizontalBox();
+            row.add(new JLabel("tu "));
+            row.add(new JTextField(8));
+            row.add(Box.createHorizontalGlue());
+            row.add(Box.createHorizontalStrut(32));
+            row.add(new JLabel("vous "));
+            row.add(new JTextField(8));
+        rowContainer.add(row);
+
+        row = Box.createHorizontalBox();
+            row.add(new JLabel("il/elle/on "));
+            row.add(new JTextField(8));
+            row.add(Box.createHorizontalGlue());
+            row.add(Box.createHorizontalStrut(32));
+            row.add(new JLabel("ils/elles "));
+            row.add(new JTextField(8));
+        rowContainer.add(row);
+
+        add(rowContainer);
+
+        pack();
         setLocationRelativeTo(parent);
     }
 
