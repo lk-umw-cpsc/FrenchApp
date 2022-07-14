42d34f19c5bf3916ceb08827848d994de530b0eb
Lauren Knight <lknight2@mail.umw.edu>
2022-07-13 23:31:55 -0400

    opaqueness set to false in attempt to fix windows bug

--- swingcustom/CustomRadioButton.java
+++ swingcustom/CustomRadioButton.java
@@ -6,6 +6,7 @@ public class CustomRadioButton extends JRadioButton {
     
     public CustomRadioButton(String text) {
         super(text);
+        setOpaque(false);
         setForeground(FontsAndColors.COLOR_DARK_FOREGROUND);
         setFont(FontsAndColors.FONT_BASIC_COMPONENT);
     }
